80c3a167f6d8e28cf34b734622a7844d07566647
yagmurigde <igdeyagmur@gmail.com>
2023-09-13 22:07:39 +0300

    KDV oranını anaparaya göre belirlediğimiz pratik

--- KDV Practice/src/Main.java
+++ KDV Practice/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Değişkenlerimizi tanımlıyoruz.
+        double anapara, kdvli, kdvtutari;
+//Scanner sayfamızı tanımlıyoruz.
+        Scanner input = new Scanner(System.in);
+        //anapara miktarını giriyoruz.
+        System.out.print("Anapara tutarını giriniz: ");
+        anapara = input.nextDouble();
+        //anapara tutarı 0 ile 1000 arasında ise KDV oranı %18; 1000 üzeri ise %8 olmalı.
+        if(anapara>0&&anapara<1000){kdvtutari=anapara*0.18;
+            System.out.println("KDV tutarı: " + kdvtutari);
+            System.out.println("KDVLi tutar: " + (anapara+kdvtutari));}
+        else if (anapara>=1000) {kdvtutari=anapara*0.08;
+            System.out.println("KDV tutarı: " + kdvtutari);
+            System.out.println("KDVLi tutar: " + (anapara+kdvtutari));}
+            
+        }
     }
-}
