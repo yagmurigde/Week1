b5b71564d8a5f0bf3f02d6cfa3d711da571b47e7
yagmurigde <igdeyagmur@gmail.com>
2023-09-14 22:36:43 +0300

    Taksimetre hesaplama

--- Taksimetre/src/Main.java
+++ Taksimetre/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int mesafe, acilisTutari=10;
+        double perKm=2.20, total;
+        Scanner yol = new Scanner (System.in);
+        System.out.print("Gideceğiniz mesafeyi giriniz: ");
+        mesafe = yol.nextInt();
+        //Toplam tutarı tanımlayalım.
+        total = mesafe*perKm;
+        total += acilisTutari;
+        total = (total<20) ? 20 : total ;
+        System.out.print("Taksimetre tutarı: " + total);
+
+
     }
 }
