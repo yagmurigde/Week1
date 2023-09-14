41bb3a7dcfc85f86c1c2b7d05b9ffc13f012f159
yagmurigde <igdeyagmur@gmail.com>
2023-09-14 23:27:58 +0300

    Vücut Kitle İndeksi

--- BMI Calculation/src/Main.java
+++ BMI Calculation/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Değişkenlerimizi tanımlıyoruz.
+        int weight;
+        double height, bmi;
+        //Scanner sınıfımızı tanımlayalım.
+        Scanner input = new Scanner(System.in);
+        //Değişkenlerimize değer atıyoruz.
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz: ");
+        height=input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        weight=input.nextInt();
+        //BMI(vücut kitle indeksi) formülünü yazıyoruz.
+        bmi = (weight) / (height*height);
+        //Sonucu yazdıralım.
+        System.out.print("Vücut Kitle Indeksiniz: " + bmi);
+
     }
 }
