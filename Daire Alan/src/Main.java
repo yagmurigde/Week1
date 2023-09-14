37453db707816b10d6e3203dfde5239e62b2c667
yagmurigde <igdeyagmur@gmail.com>
2023-09-14 23:08:17 +0300

    Daire diliminin alanı hesaplandı

--- Daire Alan/src/Main.java
+++ Daire Alan/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Değişkenlerimizi tanımlıyoruz.
+        int r, alpha;
+        double pi = 3.14, daireDilimi;
+        Scanner input = new Scanner(System.in);
+        //Kullanıcıya değerleri tanımlatalım.
+        System.out.print("Daire yarıçapını giriniz: ");
+        r = input.nextInt();
+        System.out.print("Dairenin merkez açısını giriniz: ");
+        alpha=input.nextInt();
+        //Formülümüzü yazalım.
+        daireDilimi = (pi*(r*r)*alpha) / 360;
+        System.out.print("Daire diliminin alanı: " +daireDilimi);
+
+
     }
 }
