24fdd5ca00bbc64fdf838925a43fbe12fdd16de8
yagmurigde <igdeyagmur@gmail.com>
2023-09-15 00:03:22 +0300

    Manav Kasa Programı

--- Manav Kasa Programi/src/Main.java
+++ Manav Kasa Programi/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    //Sabit değerleri ve değişkenlerimizi tanımlayalım.
+    double armut=2.14, elma=3.67 , domates=1.11 , muz=0.95 , patlican=5.00, toplam ;
+    int armutkg, elmakg, domateskg, muzkg, patlicankg;
+    Scanner input = new Scanner(System.in);
+    //Değişkenlerimiz için müşteriden hangi üründen hangi miktarda olacağını öğrenelim.
+        System.out.print("Kaç kilo armut alındı? : ");
+        armutkg=input.nextInt();
+        System.out.print("Kaç kilo elma alındı? : ");
+        elmakg=input.nextInt();
+        System.out.print("Kaç kilo domates alındı? : ");
+        domateskg=input.nextInt();
+        System.out.print("Kaç kilo muz alındı? : ");
+        muzkg=input.nextInt();
+        System.out.print("Kaç kilo patlıcan alındı? : ");
+        patlicankg=input.nextInt();
+        //Toplam tutarı hesaplayalım ve yazdıralım.
+        toplam = ((armutkg*armut)+(elmakg*elma)+(domateskg*domates)+(muzkg*muz)+(patlicankg*patlican));
+        System.out.print("Toplam tutar: " + toplam + " TL");
+
+
+
+
+
     }
 }
