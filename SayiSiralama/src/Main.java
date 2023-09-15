a787f5801a7e1bdc5f01583678e084b304a67f3f
yagmurigde <igdeyagmur@gmail.com>
2023-09-16 00:27:18 +0300

    Sayı sıralayan program

--- SayiSiralama/src/Main.java
+++ SayiSiralama/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a, b, c;
+        Scanner inp = new Scanner (System.in);
+
+        System.out.print("a değerini giriniz: ");
+        a=inp.nextInt();
+
+        System.out.print("b değerini giriniz: ");
+        b=inp.nextInt();
+
+        System.out.print("c değerini giriniz: ");
+        c=inp.nextInt();
+
+        if (a<b && a<c) { if (b<c) {
+            System.out.print("a<b<c");} else {
+            System.out.print("a<c<b");}}
+        else if (b<a && b<c){ if (a<c) {
+            System.out.print("b<a<c");} else {
+            System.out.print("b<c<a");}}
+        else {if (b<a) {
+            System.out.print("c<b<a");} else {
+            System.out.print("c<a<b");}}
+
+
+
+
+
     }
 }
