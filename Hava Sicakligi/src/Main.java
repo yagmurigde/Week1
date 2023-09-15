42e6bbc419b9e595e5df0068fe3ae8e083cbc685
yagmurigde <igdeyagmur@gmail.com>
2023-09-16 00:12:23 +0300

    Heat/Activity

--- Hava Sicakligi/src/Main.java
+++ Hava Sicakligi/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int heat;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Insert heat: ");
+        heat=inp.nextInt();
+        if (heat<5){
+            System.out.print("Acitivity name: Ski");}
+        else if (heat>5 && heat<15) {
+            System.out.print("Activity name: Cinema");}
+        else if (heat>15 && heat<25) {System.out.print("Activity name: Picnic");}
+        else {System.out.print("Activity name: Swimming");}
+
+        }
+
     }
-}
+
