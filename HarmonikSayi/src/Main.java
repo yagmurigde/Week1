3b51e62af55a098dc1e3c06de9d6f24f8f788464
yagmurigde <igdeyagmur@gmail.com>
2023-09-17 01:05:22 +0300

    harmonik sayı hesabı

--- HarmonikSayi/src/Main.java
+++ HarmonikSayi/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int number;
+        double result = 0;
+
+        Scanner scan =new Scanner(System.in);
+
+        System.out.print("Insert number: ");
+        number=scan.nextInt();
+
+        for (double i=1 ; i <= number ; i++) {
+            result += (1/i);
+        }
+        System.out.println("Result: " + result);
+
     }
 }
