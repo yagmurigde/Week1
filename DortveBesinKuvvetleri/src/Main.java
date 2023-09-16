63d25205e5c64270e1705adcaa396c22b55137f0
yagmurigde <igdeyagmur@gmail.com>
2023-09-16 22:28:40 +0300

    dört ve beşin kuvvetleri

--- DortveBesinKuvvetleri/src/Main.java
+++ DortveBesinKuvvetleri/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Insert Number: ");
+        number=inp.nextInt();
+
+        for (int i=1; i<number; i*=4) {
+            System.out.println(i);}
+        for (int j=1; j<number; j*=5) {
+            System.out.println(j);}
+
+
+
+
+
     }
 }
