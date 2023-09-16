7782b4b48b9626d57987839bd6ff30d170f95526
yagmurigde <igdeyagmur@gmail.com>
2023-09-16 21:51:32 +0300

    Dörde bölünen sayılar

--- TekSayiToplami/src/Main.java
+++ TekSayiToplami/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        int number;
+        int total = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayıyı giriniz: ");
+            number=input.nextInt();
+            if (number%2 == 0 && number%4 == 0) {total+=number;}}
+        while (number%2 ==0);
+
+        System.out.println("İşlem sonucu: " + total); }}
