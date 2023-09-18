60aa17b1cef5d96f6a4ce110c8e9341e2c051861
yagmurigde <igdeyagmur@gmail.com>
2023-09-18 21:43:47 +0300

    Max değer bulundu

--- MinMaxDegerBulma/src/Main.java
+++ MinMaxDegerBulma/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int N, sayi;
-        int enbuyuk=0;
+        int N;
+        int enbuyuk=0, enkucuk=0;
 
 
         System.out.println("Kaç tane sayı gireceksiniz? ");
@@ -15,8 +15,11 @@ public class Main {
         //Kaç sayı gireceğiz onu soruyoruz.
         for (int i = 1; i <= N; i++) {
             System.out.println(i + ". sayıyı giriniz: ");
-            sayi = scan.nextInt();
-            if (sayi>enbuyuk){enbuyuk=sayi;}}
+            int sayi = scan.nextInt();
+            if (i==1){ enbuyuk=sayi; enkucuk=sayi;}
+            if (sayi>enbuyuk){enbuyuk=sayi;}
+            if (sayi<enkucuk){enkucuk=sayi;}}
         System.out.println("En büyük sayı: " + enbuyuk);
+        System.out.println("En büyük sayı: " + enkucuk);
 
         }}
