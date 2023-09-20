b52558fe0d91c6722bbead0919086b64b3c9d661
yagmurigde <igdeyagmur@gmail.com>
2023-09-20 23:54:31 +0300

    fibonacci

--- Fibonacci/src/Main.java
+++ Fibonacci/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        //Kullanıcıdan dizinin eleman sayısını istiyoruz.
+
+        System.out.print("Insert number: ");
+        int N = input.nextInt();
+
+        //İlk iki değer biliniyor
+        int toplam0 = 0;
+        int toplam1 = 1;
+        int toplam;
+
+
+        for (int i = 2; i <= N; i++) {
+            toplam = toplam0 + toplam1;
+            System.out.print(toplam + " ");
+            toplam0 = toplam1;
+            toplam1 = toplam;
+        }
     }
 }
