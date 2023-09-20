6581431eb589b347b8d573520b6f9032d6c9edfe
yagmurigde <igdeyagmur@gmail.com>
2023-09-20 18:11:05 +0300

    hmwrk

---
+++ TersUcgen/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //N basamak sayısı olsun.
+        System.out.println("Insert number: ");
+        int N = scan.nextInt();
+
+        /*Üçgeninimiz sağ ve sol olmak üzere iki simetrik parça olarak düşünelim.
+        Bu sebeple N değerini ikiyle çarparak sağ ve sol olmak üzere iki ü.gen yapmış olduk.*/
+
+        for(int i=2*N ; i>0 ; i--) {
+            for (int b = 0; b < (2*N-i); b++) {
+                System.out.print(" ");
+            }
+        for (int y = 1 ; y< (2*i-2*N); y++) {
+            System.out.print("*");
+              }
+
+        System.out.println(" ");
+
+
+
+    }
+}}
