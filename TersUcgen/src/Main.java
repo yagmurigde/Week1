5c066ceafcbe171256785758805aacccaffda9c8
yagmurigde <igdeyagmur@gmail.com>
2023-09-20 19:28:11 +0300

    Merge remote-tracking branch 'origin/main'

--- TersUcgen/src/Main.java
+++ TersUcgen/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int N = scan.nextInt();
 
         /*Üçgeninimiz sağ ve sol olmak üzere iki simetrik parça olarak düşünelim.
-        Bu sebeple N değerini ikiyle çarparak sağ ve sol olmak üzere iki ü.gen yapmış olduk.*/
+        Bu sebeple N değerini ikiyle çarparak sağ ve sol olmak üzere iki üçgen yapmış olduk.*/
 
         for(int i=2*N ; i>0 ; i--) {
             for (int b = 0; b < (2*N-i); b++) {
