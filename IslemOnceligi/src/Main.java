93b447d3461b672a754c50c99b5de8db28239cc2
yagmurigde <igdeyagmur@gmail.com>
2023-09-21 00:14:53 +0300

    işlem önceliği

---
+++ IslemOnceligi/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Soru: Kullanıcıdan 3 tane sayı (a, b, c) alıp bu sayıları işlem
+        sırasına göre işleyen bir program yazın.
+        İşlem sırası şöyle olsun: a+b*c-b. İşlem tamamladıktan sonra sonucu ekrana
+        yazdırın.*/
+        /*Örnek: Kullanıcıdan alınan sayılar 10, 2 ve 3 olsun.
+        Yapılacak işlem ise 10+2*3-2 şeklinde olsun.*/
+
+//İpucu 1: Kullanıcıdan veri almak için `Scanner` sınıfını kullanabilirsiniz.
+
+//İpucu 2: Sonucu ekrana yazdırmak için `System.out.println()` metodunu kullanabilirsiniz.
+
+
+    }
+}
