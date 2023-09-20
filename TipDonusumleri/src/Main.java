8480b905d74cb68ce936c59b1d17f32477756aa8
yagmurigde <igdeyagmur@gmail.com>
2023-09-21 01:10:24 +0300

    commit

--- TipDonusumleri/src/Main.java
+++ TipDonusumleri/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Insert number: ");
+        int sayi1 = scan.nextInt();
+        double sayi1casting = sayi1; //integerdan doublea
+        System.out.println(sayi1casting);
+
+        System.out.print("Insert number: ");
+        double sayi2 = scan.nextDouble();
+        int sayi2casting = (int) sayi2; //doubledan integera
+        System.out.print(sayi2casting);
 
     }
 }
